44c4a543aeea861ca2cc960394aeb3e50cef83c9
Matheus Gabriel Pereira do Couto <matheus.gabriel.couto@hotmail.com>
2025-02-12 16:44:44 -0300

    adicionando a seed no projeto

---
+++ prisma/seed/seed.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from '@prisma/client'
+import { hash } from 'bcryptjs'
+
+const prisma = new PrismaClient()
+
+async function main() {
+  const hashedPassword = await hash('admin', 8)
+
+  await prisma.user.create({
+    data: {
+      name: 'admin',
+      email: 'admin@admin.com',
+      password: hashedPassword
+    }
+  })
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
