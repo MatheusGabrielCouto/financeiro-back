ddc3e8920ae27d5322ab27da99977f0479cf41ee
Matheus Gabriel Pereira do Couto <matheus.gabriel.couto@hotmail.com>
2025-02-20 09:45:33 -0300

    alterando diretório do render

--- src/controllers/future-purchase.controller.ts
+++ src/controllers/future-purchase.controller.ts
@@ -124,7 +124,7 @@ export class FuturePurchaseController {
 
      // 3️⃣ Deletar o arquivo da pasta de uploads
      const uploadsDir = process.env.NODE_ENV === 'production' 
-     ? '/mnt/data' // Diretório persistente do Render
+     ? '/var/data' // Diretório persistente do Render
      : join(process.cwd()); // Para desenvolvimento local
     const filePath = join(uploadsDir, futurePurchase.image);
 
